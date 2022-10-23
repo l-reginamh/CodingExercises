d0b77f7240e82317c64a662446f7c2296e97f305
DIR\regina.min.huei.liew <liew.reginamh@gmail.com>
2022-10-23 22:09:01 +0800

    commit exercise 001

---
+++ src/main/java/exercise001/Main.java
@@ -0,0 +1,15 @@
+package exercise001;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * 001 - Speed Converter
+         */
+        System.out.println("Speed Converter");
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(10.25);
+        SpeedConverter.printConversion(-5.6);
+        SpeedConverter.printConversion(25.42);
+        SpeedConverter.printConversion(75.114);
+    }
+}
